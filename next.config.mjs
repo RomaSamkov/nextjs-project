07a1392ed5dae8f2c00b1986839474d2f37059c5
RomaSamkov <Romansamkov.rs@gmail.com>
2026-07-22 17:04:41 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export", // Генерує статичні файли в папку out
+  images: {
+    unoptimized: true, // Потрібно для роботи <Image /> на статичному хостингу
+  },
+  basePath: "/nextjs-project",
 };
 
 export default nextConfig;
